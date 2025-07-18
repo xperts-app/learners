24e3be68f90fec5a9ae8c086d451c0b77ace08c3
xperts-app <xperts.data1@gmail.com>
2025-07-18 10:40:34 +0530

    Create data.js

---
+++ cp/data.js
@@ -0,0 +1,32 @@
+// data.js
+
+const academicData = {
+    "Class 11": {
+        "Physics": [
+            "Units and Measurements",
+            "Motion in a Straight Line",
+            "Laws of Motion"
+        ],
+        "Social Science": [
+            "Agriculture",
+            "The French Revolution",
+            "India: Size and Location"
+        ],
+        "Botany": [
+            "Cell: The Unit Of Life",
+            "Biomolecules",
+            "Cell Cycle And Cell Division",
+            "The Living World"
+        ]
+    },
+    "Class 12": {
+        "Physics": [
+            "Electric Charges and Fields",
+            "Electrostatic Potential and Capacitance"
+        ],
+        "Botany": [
+            "Reproduction in Organisms",
+            "Sexual Reproduction in Flowering Plants"
+        ]
+    }
+};
