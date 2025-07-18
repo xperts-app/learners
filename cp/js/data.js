e7bb29b240be378cda10da58acfb3499bce35894
xperts-app <xperts.data1@gmail.com>
2025-07-18 10:12:54 +0530

    Create data.js

---
+++ cp/js/data.js
@@ -0,0 +1,40 @@
+// js/data.js
+
+const academicData = {
+    "Class 11": {
+        "Physics": [
+            "Units and Measurements",
+            "Motion in a Straight Line",
+            "Laws of Motion"
+        ],
+        "Social Science": [
+            "Agriculture",
+            "The French Revolution",
+            "India: Size and Location"
+        ],
+        "Zoology": [
+            "Structural Organisation in Animals",
+            "Animal Kingdom"
+        ],
+        "Botany": [
+            "Cell: The Unit Of Life",
+            "Biomolecules",
+            "Cell Cycle And Cell Division",
+            "The Living World",
+            "Biological Classification",
+            "Morphology Of Flowering Plants",
+            "Anatomy Of Flowering Plants",
+            "Photosynthesis In Higher Plants"
+        ]
+    },
+    "Class 12": {
+        "Physics": [
+            "Electric Charges and Fields",
+            "Electrostatic Potential and Capacitance"
+        ],
+        "Botany": [
+            "Reproduction in Organisms",
+            "Sexual Reproduction in Flowering Plants"
+        ]
+    }
+};
